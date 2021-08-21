550f404f5db04ced65d4b689a833d589ae36850d
allgoodworks <allgoodworks79@gmail.com>
2021-08-21 03:17:17 -0400

    leaderboard page

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { useState, useCallback} from 'react';
+import { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import Layout from "../components/Layout";
 import LeaderBoardCard from '../components/LeaderBoardCard';
@@ -7,12 +7,17 @@ import StyledInput from '../components/StyledInput';
 import Carousel from '../components/Carousel';
 import { useQuery } from "@apollo/client";
 import { throttle } from 'lodash';
-import {QUERY_USER} from '../dgraph/queries/user';
+import { QUERY_USER } from '../dgraph/queries/user';
 import { LeaderboardType } from '../constants';
+import { useWallet } from '../utils/wallet';
+const FETCH_LIMIT = 15;
 
-const FETCH_LIMIT  = 15;
-
-
+const PageWrapper = styled.main`
+  align-items: center;
+  justify-content: center;
+  padding-top: 2%;
+  margin: 0 5%;
+`;
 const Container = styled.div`
   height: 100vh;
   overflow-y: scroll;
@@ -59,44 +64,46 @@ const SearchBarWrapper = styled.div`
 
 export default function LeaderBoard() {
   const [leaderboardType, setLeaderBoardType] = useState<LeaderboardType>(LeaderboardType.Weekly);
-  const [rankings, setUserRankings] = useState<any>([]);
+  const [rankings, setUserRankings] = useState<any>([1, 2, 3]);
   const [allFetched, setAllFetched] = useState<boolean>(false);
   const [offset, setOffset] = useState<number>(0);
- 
-  const { loading, error, called, fetchMore} = useQuery(QUERY_USER, 
-    {variables: {first: FETCH_LIMIT, offset, order: { asc: 'currentRank' }, filter: { currentRank: { gt: 0 } } }, 
-    onCompleted: (data) =>{
-    if(called && data?.queryUser.length < FETCH_LIMIT) {
-      setAllFetched(true);
-    }
-    setUserRankings([...rankings, ...data?.queryUser]);
-  }});
+  const { connected } = useWallet();
+  const { loading, error, called, fetchMore } = useQuery(QUERY_USER,
+    {
+      variables: { first: FETCH_LIMIT, offset, order: { asc: 'currentRank' }, filter: { currentRank: { gt: 0 } } },
+      onCompleted: (data) => {
+        if (called && data?.queryUser.length < FETCH_LIMIT) {
+          setAllFetched(true);
+        }
+        setUserRankings([...rankings, ...data?.queryUser]);
+      }
+    });
 
   const withThrottleBoardScroll = useCallback(
     throttle((totalHeight, currentHeigth) => {
-      if (currentHeigth >= totalHeight && !loading  && !allFetched) {
+      if (currentHeigth >= totalHeight && !loading && !allFetched) {
         const newOffset = offset + FETCH_LIMIT;
-        fetchMore({variables: {offset: newOffset }});
+        fetchMore({ variables: { offset: newOffset } });
         setOffset(newOffset);
       }
     }, 1000),
     [offset, loading, fetchMore, allFetched],
   );
 
-    const onScroll = (event:any) => {
-      const maxScrollPosition= (event.currentTarget.scrollHeight - event.currentTarget.clientHeight) * 0.75;
-      const scrollBarPosition =event.currentTarget.scrollTop;
-        withThrottleBoardScroll(maxScrollPosition,scrollBarPosition);
-    }
+  const onScroll = (event: any) => {
+    const maxScrollPosition = (event.currentTarget.scrollHeight - event.currentTarget.clientHeight) * 0.75;
+    const scrollBarPosition = event.currentTarget.scrollTop;
+    withThrottleBoardScroll(maxScrollPosition, scrollBarPosition);
+  }
 
-  if(!called) {
+  if (!called) {
     return <p>loading</p>;
   }
 
   if (error) {
     return <p>Error fetching data</p>
   }
-  
+
   return (
     <Container onScroll={onScroll}>
       <Head>
@@ -105,29 +112,111 @@ export default function LeaderBoard() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-      <Main>
-        <Carousel />
-        <ContentWrapper>
-          <div className="title">Leaderboard</div>
-          <WeeklySeasonalToggle>
-            <ClearButton onClick={() => setLeaderBoardType(LeaderboardType.Weekly)} selected={leaderboardType === LeaderboardType.Weekly}>
-              Weekly
-            </ClearButton>
-            <div>|</div>
-            <ClearButton onClick={() => setLeaderBoardType(LeaderboardType.Seasonal)} selected={leaderboardType === LeaderboardType.Seasonal}>
-              Seasonal
-            </ClearButton>
-          </WeeklySeasonalToggle>
-          <SearchBarWrapper>
-            <StyledInput />
-          </SearchBarWrapper>
-          {rankings.map((user: User) => {
-            return(
-              <LeaderBoardCard key={user.discordId} user={user} />
-            )
-          })}
-        </ContentWrapper>
-      </Main>
+        <PageWrapper className={connected ? 'height-f' : 'height-v'}>
+          <Carousel />
+          <ContentWrapper>
+            <div className="title">Leaderboard</div>
+            <WeeklySeasonalToggle>
+              <ClearButton onClick={() => setLeaderBoardType(LeaderboardType.Weekly)} selected={leaderboardType === LeaderboardType.Weekly}>
+                Weekly
+              </ClearButton>
+              <div>|</div>
+              <ClearButton onClick={() => setLeaderBoardType(LeaderboardType.Seasonal)} selected={leaderboardType === LeaderboardType.Seasonal}>
+                Seasonal
+              </ClearButton>
+            </WeeklySeasonalToggle>
+            <SearchBarWrapper>
+              <StyledInput />
+            </SearchBarWrapper>
+            <div className="row no-gutters leader-row mb-3 py-2">
+              <div className="col-md-4  d-none d-md-block ">
+                <div className="d-flex justify-content-md-start">
+                  <b>Holders</b><img src='/sort_icon.svg' alt='sort-icon' />
+                </div>
+
+              </div>
+              <div className="col-md-5  d-none d-md-block ">
+                <div className="row no-gutters h-100">
+                  <div className="col-md-4">
+                    <div className="d-flex justify-content-center align-items-center h-100 w-100">
+                      <div className="align-self-center">
+                        <b>Ninja</b>
+                      </div>
+
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="d-flex justify-content-center align-items-center h-100 w-100">
+                      <div className="align-self-center">
+                        <b>24h</b>
+                      </div>
+
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="d-flex justify-content-center align-items-center h-100 w-100">
+                      <div className="align-self-center">
+                        <b>7d</b>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2  d-none d-md-block ">
+                <div className="d-flex justify-content-center align-items-center h-100 w-100">
+                  <div className="align-self-center">
+                    <b>Rewards</b>
+                  </div>
+
+                </div>
+              </div>
+              <div className="col-md-1  d-none d-md-block ">
+                <div className="d-flex justify-content-center align-items-center h-100 w-100">
+
+
+                </div>
+              </div>
+            </div>
+            {rankings.map((user: User) => {
+              return (
+                <LeaderBoardCard key={user.discordId} user={user} />
+              )
+            })}
+            <div className="container-fluid">
+              <div className="row no-gutters">
+                <div className="col-12">
+                  <nav aria-label="Page navigation example">
+                    <div className="d-flex justify-content-md-end justify-content-center">
+                      <ul className="pagination">
+                        <li className="page-item bg-dark">
+                          <a className="page-link" href="#" aria-label="Previous">
+                            <i className="fas fa-chevron-left" style={{ color: 'white' }}></i>
+                            <span className="sr-only">Previous</span>
+                          </a>
+                        </li>
+                        <li className="page-item"><a className="page-link" href="#" style={{ color: '#851cef !important' }}>1</a></li>
+                        <li className="page-item"><a className="page-link" href="#">2</a></li>
+                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                        <li className="page-item"><a className="page-link" href="#">4</a></li>
+                        <li className="page-item"><a className="page-link" href="#">5</a></li>
+                        <li className="page-item"><a className="page-link" href="#">...</a></li>
+                        <li className="page-item"><a className="page-link" href="#">100</a></li>
+                        <li className="page-item">
+                          <a className="page-link" href="#" aria-label="Next">
+                            <i className="fas fa-chevron-right" style={{ color: 'white' }}></i>
+                            <span className="sr-only">Next</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+
+          </ContentWrapper>
+        </PageWrapper>
       </Layout>
     </Container>
   );
